015a1531b5a8aef1bd66a004611955be7fa7908a
iotareal <you@example.com>
2024-09-26 17:10:26 +0530

    lol

--- frontend/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return <div className="bg-gray-300 p-4">
-        <div className="text-2xl font-bold flex gap-2 justify-center">
-            &copy; 2024, Sample Footer Text
-        </div>
-    </div>
-}
-
-export default Footer;
--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../components/Home/Hero";
-
-const Home = () => {
-    return <div className="flex p-10 min-h-screen items-center">
-        <Hero />
-    </div>;
-}
-
-export default Home;
